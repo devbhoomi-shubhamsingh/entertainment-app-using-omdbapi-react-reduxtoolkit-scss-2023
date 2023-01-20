eeb9a3fca39c2c02599d559469dbfb3822114086
Shubham Singh <devbhoomi.shubhamsingh@gmail.com>
2023-01-20 20:46:13 +0530

    Entertainment App Development Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+//For Routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <Router>
+          <Header />
+          <div className="container">
+            {/* React Router DOM Version 6 => Routing Start  */}
+
+            {/* <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="movies" element={<Home />}>
+                <Route path=":imdbId" element={<MovieDetails />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes> */}
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies" element={<Home />} />
+              <Route path="/movies/:imdbId" element={<MovieDetails />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            {/* Routing Finish  */}
+          </div>
+          <Footer />
+        </Router>
+      </div>
+    </>
   );
 }
 
